b6b5a041ff4344ef70ce39b7472deabc98d32186
Angel Vladov <angel.vladov@d-consult.net>
2017-10-29 00:43:20 +0300

    recompiled settings

--- src/jqextend.js
+++ src/jqextend.js
@@ -9,6 +9,7 @@
     ////////////////////
 
     function $extendName$() {
+        // TODO: Replace with your code here
         return this.each(function() {
             this.checked = true;
         });
